067601d7493ed7a367554a06c2c601848df5c99b
Niels Pieters <dennisvth@hotmail.com>
2018-01-24 13:59:13 +0100

    Added noisegenerator for mapGen

---
+++ Assets/Editor/mapGeneratrionEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof (mapGeneration))]
+
+public class mapGeneratrionEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        mapGeneration mapGen = (mapGeneration)target;
+
+        if (DrawDefaultInspector()){
+            if (mapGen.autoUpdate)
+            {
+             mapGen.generateMap();
+            }
+        }
+
+        if (GUILayout.Button ("Generate"))
+        {
+            mapGen.generateMap();
+        }
+
+    }
+}
---
+++ Assets/Script/mapGeneration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGeneration : MonoBehaviour {
+	
+	public int mapHeight;
+	public int mapWidth;
+	public float noiseScale;
+
+    public bool autoUpdate;
+
+	public void generateMap(){
+		
+		float[,] noiseMap = noise.generateNoiseMap (mapWidth, mapHeight, noiseScale);
+
+		mapDisplay display = FindObjectOfType<mapDisplay> ();
+		display.DrawNoiseMap (noiseMap);
+
+	}
+}
