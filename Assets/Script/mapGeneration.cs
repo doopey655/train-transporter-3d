83d68eb50dd47fe8cef28db47b696234644ba291
Niels Pieters <dennisvth@hotmail.com>
2018-01-24 16:03:36 +0100

    Refined noise generator

--- Assets/Script/mapGeneration.cs
+++ Assets/Script/mapGeneration.cs
@@ -8,14 +8,42 @@ public class mapGeneration : MonoBehaviour {
 	public int mapWidth;
 	public float noiseScale;
 
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
 	public void generateMap(){
 		
-		float[,] noiseMap = noise.generateNoiseMap (mapWidth, mapHeight, noiseScale);
+		float[,] noiseMap = noise.generateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
 		mapDisplay display = FindObjectOfType<mapDisplay> ();
 		display.DrawNoiseMap (noiseMap);
 
 	}
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+    }
 }
